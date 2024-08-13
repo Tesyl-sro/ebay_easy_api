8b4a19052df1023b958db567e7f1ef53f3c555c4
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-13 08:51:47 +0200

    add test for `find_item()`

---
+++ tests/item_details.rs
@@ -0,0 +1,16 @@
+use ebay_easy_api::{models::Marketplace, EbayApiClient};
+use std::env::var;
+
+#[test]
+fn item_details() {
+    let client =
+        EbayApiClient::new_unchecked(var("EBAY_TOKEN").unwrap(), Marketplace::UnitedStates);
+
+    let browser = client.search();
+    let results = browser.search("gaming pc", 2).unwrap();
+
+    let first = results.iter().next().unwrap();
+    let details = browser.find_item(&first.id).unwrap();
+
+    assert!(details.is_some());
+}
