47678260ba65bf844e4fb1b3c558c2f4d1e71e36
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-13 14:05:21 +0200

    add missing regions

--- src/models/marketplace.rs
+++ src/models/marketplace.rs
@@ -24,6 +24,32 @@ pub enum Marketplace {
     UnitedKingdom,
     #[serde(rename = "EBAY_FR")]
     France,
+    // Could be 210 (French) or 2 (English)
+    #[serde(rename = "EBAY_CA")]
+    Canada,
+    #[serde(rename = "EBAY_AU")]
+    Australia,
+    #[serde(rename = "EBAY_AT")]
+    Austria,
+    // Could be 123 (Dutch) or 23 (French)
+    #[serde(rename = "EBAY_BE")]
+    Belgium,
+    #[serde(rename = "EBAY_ES")]
+    Spain,
+    #[serde(rename = "EBAY_CH")]
+    Switzerland,
+    #[serde(rename = "EBAY_NL")]
+    Netherlands,
+    #[serde(rename = "EBAY_HK")]
+    HongKong,
+    #[serde(rename = "EBAY_IN")]
+    India,
+    #[serde(rename = "EBAY_MY")]
+    Malaysia,
+    #[serde(rename = "EBAY_PH")]
+    Philippines,
+    #[serde(rename = "EBAY_PL")]
+    Poland,
 }
 
 /// An error representing a failed conversion from a string to a marketplace ID.
@@ -52,6 +78,18 @@ impl Marketplace {
             Self::Singapore => "EBAY_SG",
             Self::UnitedKingdom => "EBAY_UK",
             Self::France => "EBAY_FR",
+            Self::Canada => "EBAY_CA",
+            Self::Australia => "EBAY_AU",
+            Self::Austria => "EBAY_AT",
+            Self::Belgium => "EBAY_BE",
+            Self::Spain => "EBAY_ES",
+            Self::Switzerland => "EBAY_CH",
+            Self::Netherlands => "EBAY_NL",
+            Self::HongKong => "EBAY_HK",
+            Self::India => "EBAY_IN",
+            Self::Malaysia => "EBAY_MY",
+            Self::Philippines => "EBAY_PH",
+            Self::Poland => "EBAY_PL",
         }
     }
 
@@ -65,6 +103,18 @@ impl Marketplace {
             Self::Singapore => "Singapore",
             Self::UnitedKingdom => "United Kingdom",
             Self::France => "France",
+            Self::Canada => "Canada",
+            Self::Australia => "Australia",
+            Self::Austria => "Austria",
+            Self::Belgium => "Belgium",
+            Self::Spain => "Spain",
+            Self::Switzerland => "Switzerland",
+            Self::Netherlands => "Netherlands",
+            Self::HongKong => "Hong",
+            Self::India => "India",
+            Self::Malaysia => "Malaysia",
+            Self::Philippines => "Philippines",
+            Self::Poland => "Poland",
         }
     }
 }
