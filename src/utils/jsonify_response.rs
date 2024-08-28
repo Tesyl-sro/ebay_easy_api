7234da57ab69335da8e74e4ab287c2cc8c12322a
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-28 11:18:17 +0200

    use `serde_json::from_slice`

--- src/utils/jsonify_response.rs
+++ src/utils/jsonify_response.rs
@@ -1,13 +1,19 @@
 use crate::error::Result;
 use reqwest::blocking::Response;
 use serde::de::DeserializeOwned;
+use std::io::Read;
 
 pub trait Jsonify {
     fn jsonify<T: DeserializeOwned>(self) -> Result<T>;
 }
 
 impl Jsonify for Response {
-    fn jsonify<T: DeserializeOwned>(self) -> Result<T> {
-        Ok(serde_json::from_reader(self)?)
+    fn jsonify<T: DeserializeOwned>(mut self) -> Result<T> {
+        // A search result with 200 items is ~310kB.
+        // This should prevent some allocations.
+        let mut buf = Vec::with_capacity(320 * 1024);
+        self.read_to_end(&mut buf).unwrap();
+
+        Ok(serde_json::from_slice(&buf)?)
     }
 }
